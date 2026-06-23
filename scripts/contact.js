2b5f7967ad873148b0e618977e6eb7820910fb9f
Dueben Presley Lewis <duebenlewis1000@gmail.com>
2026-06-23 10:14:42 +0530

    added toast notification feature for email

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,9 @@ import {linkToHome} from './utility/home.js';
 
 linkToHome();
 
+const textarea = document.getElementById("user-message");
+
+textarea.addEventListener("input", () => {
+    textarea.style.height = "auto";               // Reset height
+    textarea.style.height = textarea.scrollHeight + "px"; // Set new height
+});
